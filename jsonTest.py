e65d999a01681914a57d2b0d1322c502b8958346
Thomas Perz <perz@dhcp-gs-1610.redrover.cornell.edu>
2015-04-19 13:21:12 -0400

    Updated JSONs/CSVs with Other Studios
    Replaced Studios with <2% of total yearly revenue with the Other
    Studios name in the CSVs, recreated JSONs

--- jsonTest.py
+++ jsonTest.py
@@ -1,10 +1,13 @@
 import re
 
+#Get data from the stringified +copy/pasted genJSON result for some year
 with open('stringified','r') as u:
 	jsonData = u.read()
 
 result = re.sub(r'{("name":"[^"]+"),"children":\[{"Studio":"[^"]+","Genre":"[^"]+","Film":"[^"]+",("Gross":[^}]+)}\]}', r'{\1,\2}',jsonData)
 
-with open('fixedJson2007.json','w') as f:
+#Write for year
+with open('2007.json','w') as f:
 	f.write(result)
 
+#Clean up with jsonlint.com and remove null values (caused by csv oddities)
